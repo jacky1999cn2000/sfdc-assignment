081ee21b830c0dc7d951f2d352ca6e9406966819
Jacky Zhao <jacky.zhao@bunchball.com>
2017-01-30 15:26:02 -0800

    map marker, search feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,14 +17,11 @@ function countSelection(){
   });
 }
 
-// toggle checkboxes' status when clicking the "select all" checkbox
+// update checkboxes' statuses when clicking the "select all" checkbox
 function selectAll(){
-  console.log('selectAll');
-
   countSelection();
 
-  console.log('checkedNumber ',checkedNumber);
-
+  // select all or unselect all
   if(checkedNumber == availableCheckboxes()){
     document.querySelectorAll("input[type='checkbox']").forEach(function(element){
       element.checked = false;
@@ -34,23 +31,25 @@ function selectAll(){
       element.checked = true;
     });
   }
+
   saveSelection();
 }
 
-// sync selectedIdArray based on checkboxes' status
+// update selectedIdArray based on checkboxes
 function saveSelection(){
-  console.log('saveSelection');
-
   selectedIdString = selectedIdArray.join(' ');
 
   document.querySelectorAll("input[type='checkbox']").forEach(function(element){
     if(element.checked){
+      // need to exclude "select all" checkbox
       if(element.id != 'selectallsightingscheckbox' && selectedIdString.indexOf(element.id) == -1){
         selectedIdArray.push(element.id);
         selectedIdString = selectedIdString + ' ' + element.id;
       }
     }else{
+      // if there was one unchecked checkbox, then uncheck "select all" checkbox
       document.getElementById('selectallsightingscheckbox').checked = false;
+
       if(selectedIdString.indexOf(element.id) != -1){
         for(i=0; i<selectedIdArray.length; i++){
           if(selectedIdArray[i] == element.id){
@@ -65,18 +64,26 @@ function saveSelection(){
   console.log('selectedIdArray ',selectedIdArray);
 }
 
+// restore checked checkboxes based on selectedIdArray
 function restoreSelection(){
-  console.log('restoreSelection');
-
   selectedIdString = selectedIdArray.join(' ');
+
+  // check corresponding checkbox if its id was found in selectedIdString
   document.querySelectorAll("input[type='checkbox']").forEach(function(element){
     if(selectedIdString.indexOf(element.id) != -1){
       element.checked = true;
     }
   });
 
+  // if all checkboxes checked on the page, then check "select all" checkbox
   countSelection();
   if(checkedNumber == availableCheckboxes()){
     document.getElementById('selectallsightingscheckbox').checked = true;
   }
+}
+
+// clear all selections after clicking search button
+function clearSelection(){
+  selectedIdArray = [];
+  selectedIdString = '';
 }
