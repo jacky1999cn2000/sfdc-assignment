cdcc1c6fef9b0ef96be52fac5e888fc2d0f108b8
Jacky Zhao <jacky.zhao@bunchball.com>
2017-02-01 00:15:48 -0800

    validate selected sighting records and refresh corresponding page blocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,20 @@
-// array to store all selected sighting ids
-var selectedIdArray = [];
-var selectedIdString,checkedNumber;
+/*
+  selectedIdArray: array to store all selected sighting Ids
+  selectedIdString: sighting id string converted from selectedIdArray and delimited by space
+  checkedNumber: the number of selected checkboxes on current page
+*/
+var selectedIdArray = [], selectedIdString, checkedNumber;
 
-// calculate number of checkboxes on current page (exclude "select all" checkbox)
+/*
+  count how many checkboxes on the current page (exclude the "select all" checkbox)
+*/
 function availableCheckboxes(){
   return document.querySelectorAll("input[type='checkbox']").length - 1;
 }
 
-// count how many checkboxes were checked on current page (exclude "select all" checkbox)
+/*
+  count how many checkboxes were checked on current page (exclude the "select all" checkbox)
+*/
 function countSelection(){
   checkedNumber = 0;
   document.querySelectorAll("input[type='checkbox']").forEach(function(element){
@@ -17,11 +24,11 @@ function countSelection(){
   });
 }
 
-// update checkboxes' statuses when clicking the "select all" checkbox
+/*
+  toggle all checkboxes on current page when the "select all" checkbox was clicked
+*/
 function selectAll(){
   countSelection();
-
-  // select all or unselect all
   if(checkedNumber == availableCheckboxes()){
     document.querySelectorAll("input[type='checkbox']").forEach(function(element){
       element.checked = false;
@@ -31,23 +38,23 @@ function selectAll(){
       element.checked = true;
     });
   }
-
   saveSelection();
 }
 
-// update selectedIdArray based on checkboxes
+/*
+  sync selectedIdArray and selectedIdString with checkboxes on the current page
+*/
 function saveSelection(){
   selectedIdString = selectedIdArray.join(' ');
 
   document.querySelectorAll("input[type='checkbox']").forEach(function(element){
     if(element.checked){
-      // need to exclude "select all" checkbox
       if(element.id != 'selectallsightingscheckbox' && selectedIdString.indexOf(element.id) == -1){
         selectedIdArray.push(element.id);
         selectedIdString = selectedIdString + ' ' + element.id;
       }
     }else{
-      // if there was one unchecked checkbox, then uncheck "select all" checkbox
+      // as long as there were unchecked checkbox, uncheck the "select all" checkbox
       document.getElementById('selectallsightingscheckbox').checked = false;
 
       if(selectedIdString.indexOf(element.id) != -1){
@@ -61,14 +68,22 @@ function saveSelection(){
     }
   });
 
+  // if all checkboxes checked on the page, then check "select all" checkbox
+  countSelection();
+  if(checkedNumber == availableCheckboxes()){
+    document.getElementById('selectallsightingscheckbox').checked = true;
+  }
+
   console.log('selectedIdArray ',selectedIdArray);
+  console.log('selectedIdString ',selectedIdString);
 }
 
-// restore checked checkboxes based on selectedIdArray
-function restoreSelection(){
+/*
+  restore checked checkboxes based on selectedIdArray
+*/
+function restoreSelection(){ console.log('restoreSelection');console.log('selectedIdArray ',selectedIdArray);
   selectedIdString = selectedIdArray.join(' ');
 
-  // check corresponding checkbox if its id was found in selectedIdString
   document.querySelectorAll("input[type='checkbox']").forEach(function(element){
     if(selectedIdString.indexOf(element.id) != -1){
       element.checked = true;
@@ -82,7 +97,9 @@ function restoreSelection(){
   }
 }
 
-// clear all selections after clicking search button
+/*
+  clear all selections after clicking search button
+*/
 function clearSelection(){
   selectedIdArray = [];
   selectedIdString = '';
--- notes/TechLearnings.md
+++ notes/TechLearnings.md
@@ -18,4 +18,29 @@
   cd assets
   ngrok http 3000
 ```
-![1.png](/notes/imgs/1.png) 
+![1.png](/notes/imgs/1.png)
+
+* [Pagination with StandardSetController](https://hisrinu.wordpress.com/2012/01/09/pagination-using-standardsetcontroller/)
+
+* [Pagination with surviving checkbox status](http://stackoverflow.com/questions/14823107/how-to-find-out-which-checkboxes-have-been-selected-on-the-next-page-in-visualfo/14825189#14825189)
+
+* For `<apex:commandButton/>`:
+  * if didn't provide a `reRender` attribute, then clicking the button will cause a full page refresh (no matter the corresponding apex function return pageReference or not), and data stored in Javascript will be lost due to it, e.g. `<apex:commandButton action="{!reset}" value="Reset"/>`
+  * if provide a `reRender` attribute, then clicking the button will cause a ajax call (only the designated part of the page being refreshed), and data stored in Javascript will be persisted, e.g. `<apex:commandButton reRender="overview,details" action="{!reset}" value="Reset"/>`
+  * of course, if you want to restore status after click the button (status was stored in Javascript data), you can use `oncomplete` attribute, e.g. `<apex:commandButton reRender="overview,details" action="{!reset}" value="Reset" oncomplete="restoreSelection()"/>`
+
+* When trying to use `onclick` attribute in `<apex:commandButton/>` to call a javascript function defined by `<apex:actionFunction/>` which in turn call a function defined in apex controller, e.g. `<apex:commandButton onclick="approve(selectedIdString);clearSelection();return false;" value="Approve"/>`, then there are some hidden issues:
+  * first, understand the race condition if not use `return false;` or `reRender`:
+    * [article 1](http://salesforce.stackexchange.com/questions/101431/race-condition-between-commandlink-and-actionfunction)
+    * [article 2](http://stackoverflow.com/questions/11893895/apexactionfunction-and-database-update-unintended-page-refresh)
+  * so, we used the `return false;` to avoid the default full page reload behavior; however, we need to refresh parts of the the page, and this was done by setting `reRender` attribute in `<apex:actionFunction/>` (see below)
+  * in addition, in order to pass parameter into the function defined by <apex:actionFunction/>, you have to provide a `reRender` attribute, and in this case, we rerender `overview` and `details` pageBlocks.
+  ```
+  <apex:actionFunction action="{!approve}" name="approve" reRender="overview,details">
+    <apex:param name="sightingIds" assignTo="{!sightingIds}" value="" />
+  </apex:actionFunction>
+  ```
+
+* [Delete classes & triggers from Production Organizaiton via Workbench](http://www.salesforceben.com/way-to-delete-apex-classes-from-production/)
+
+* [Detailed explanations about different messaging options in Visualforce](http://salesforce.stackexchange.com/questions/8139/difference-between-the-multiple-messaging-options-in-visualforce)
